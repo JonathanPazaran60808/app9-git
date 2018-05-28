94f51c6270947ef8a81380fdea6e9ea2937c7f7d
JonathanPazaran60808 <jonathandanielcruzpazaran@gmail.com>
2018-05-28 16:33:43 -0500

    9subida
    novena subida

--- js/acciones.js
+++ js/acciones.js
@@ -26,12 +26,12 @@ var options = {
 	var watchID = navigator.geolocation.getCurrentPosition(onSuccess,onError,options);
 	function onSuccess(position){
 		
-		alert('Latitud:'          +position.coords.latitude       +'\n'+
-		'Longitud:'               +position.coords.longitude      +'\n'+
-		'Altitud:'                +position.coords.altitude       +'\n'+
+		alert('Latitud:'          +position.coords.latitud      +'\n'+
+		'Longitud:'               +position.coords.longitud     +'\n'+
+		'Altitud:'                +position.coords.altitud       +'\n'+
 		'Accuracy:'                +position.coords.accuracy       +'\n'+
-		'Alt.Accuracy:'       +position.coords.altitudeAccuracy      +'\n'+
-		'Heading:'                 +position.coords.heading        +'\n'+
+		'Alt.Accuracy:'       +position.coords.aaccuracy      +'\n'+
+		'Heading:'                 +position.coords.headingg       +'\n'+
 		'Speed:'                   +position.coords.speed          +'\n'+
 		'Timestamp:'               +position.timestamp             +'\n');
 	};
@@ -51,12 +51,12 @@ var options = {
 var watchID = navigator.geolocation.watchPosition(onSuccess,onError,options);
 function onSuccess(position){
 
-	$('#latitud').html(position.coords.latitude);
-	$('#longitud').html(position.coords.longitude);
-	$('#altitud').html(position.coords.altitude);
+	$('#latitud').html(position.coords.latitud);
+	$('#longitud').html(position.coords.longitud);
+	$('#altitud').html(position.coords.altitud);
 	$('#accuracy').html(position.coords.accuracy);
-	$('#aaccuracy').html(position.coords.altitudeAccuracy);
-	$('#headingg').html(position.coords.speed);
+	$('#aaccuracy').html(position.coords.aaccuracy );
+	$('#headingg').html(position.coords.headingg);
 	$('#speed').html(position.coords.speed);
 	$('#timestamp').html(position.Timestamp);
 };
