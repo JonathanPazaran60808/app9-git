9ed484a1359a8e0301e4350381e4b05d4eaf260a
JonathanPazaran60808 <jonathandanielcruzpazaran@gmail.com>
2018-05-28 16:25:50 -0500

    9subida
    novena subida

--- js/acciones.js
+++ js/acciones.js
@@ -26,11 +26,11 @@ var options = {
 	var watchID = navigator.geolocation.getCurrentPosition(onSuccess,onError,options);
 	function onSuccess(position){
 		
-		alert('Latitude:'          +position.coords.latitude       +'\n'+
-		'Longitude:'               +position.coords.longitude      +\'n'+
-		'Altitude:'                +position.coords.altitude       +'\n'+
+		alert('Latitud:'          +position.coords.latitude       +'\n'+
+		'Longitud:'               +position.coords.longitude      +'\n'+
+		'Altitud:'                +position.coords.altitude       +'\n'+
 		'Accuracy:'                +position.coords.accuracy       +'\n'+
-		'Altitude Accuracy:'       +position.coords.altitudeAccuracy      +'\n'+
+		'Alt.Accuracy:'       +position.coords.altitudeAccuracy      +'\n'+
 		'Heading:'                 +position.coords.heading        +'\n'+
 		'Speed:'                   +position.coords.speed          +'\n'+
 		'Timestamp:'               +position.timestamp             +'\n');
